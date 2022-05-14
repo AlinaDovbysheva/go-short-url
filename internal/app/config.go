b4282f5c77e4b406a29c68f1f5fc768144c92671
AlinaDovbysheva <Dovbysheva@gmail.com>
2022-05-14 19:01:23 +0300

    inc4

--- internal/app/config.go
+++ internal/app/config.go
@@ -1,8 +1,9 @@
 package app
 
 import (
-	"flag"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 var ServerURL = "localhost:8080"
@@ -15,9 +16,18 @@ type Config struct {
 
 func (c *Config) ConfigServerEnv() {
 
-	flag.IntVar(&c.port, "SERVER_PORT", 8080, "port to listen on")
-	flag.StringVar(&c.host, "SERVER_HOST", "localhost", "host to listen on")
-	flag.Parse()
+	//flag.IntVar(&c.port, "port", 8080, "port to listen on")
+	//flag.StringVar(&c.host, "host", "localhost", "host to listen on")
+	//flag.Parse()
+	var err error
+	c.port, err = strconv.Atoi(os.Getenv("SERVER_PORT"))
+	if err != nil {
+		c.port = 8080
+	}
+	c.host = os.Getenv("SERVER_HOST")
+	if c.host == "" {
+		c.host = "localhost"
+	}
 
 	addr := fmt.Sprintf("%s:%d", c.host, c.port)
 	fmt.Printf("server start on %s\n", addr)
